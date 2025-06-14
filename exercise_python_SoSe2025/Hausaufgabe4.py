1df3040427a5b8e6319ea37b33d7254af47676fd
Theodor Guerrier <theodorguerrier@gmail.com>
2025-06-14 12:04:23 +0200

    Hausaufgabe4.py

--- exercise_python_SoSe2025/Hausaufgabe4.py
+++ exercise_python_SoSe2025/Hausaufgabe4.py
@@ -9,20 +9,20 @@ Dies ist eine temporäre Skriptdatei.
 import yfinance as yf
 import pandas as pd
 
-# Lade Tesla-Daten für 2022
+# Tesla-Daten 2022
 df = yf.download("TSLA", start="2022-01-01", end="2022-12-31")
 
-# Nur Schlusskurs
+# Schlusskurs
 df = df[['Close']]
 
-# Vorschau und Form
+# Vorschau
 print(df.head())
 print(df.shape)  # sollte (250, 1) sein
 
-# Speichern als CSV
+# Speichern
 df.to_csv("tesla_2022_close.csv")
 import matplotlib.pyplot as plt
-# Stelle den Schlusskurs (Close) über die Zeit dar
+# Stellt Schlusskursüber die Zeit dar
 df.plot(title="Tesla Schlusskurs 2022", figsize=(10, 5))
 plt.xlabel("Datum")
 plt.ylabel("Schlusskurs (USD)")
